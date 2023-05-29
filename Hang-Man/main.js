ed0e9921f091074fc266bb85c99f254c6b4738c3
sweekriti <sweekritiparajuli5@gmail.com>
2023-05-29 17:47:36 +0545

    hangman-alertbox

--- Hang-Man/main.js
+++ Hang-Man/main.js
@@ -1,6 +1,6 @@
 var word_arr = ["Snake" ,"Elephant" , "Cat"];
 var word = word_arr[Math.floor(Math.random()*word_arr.length)];
-
+let letters = [];
 
 let mainContainer = Container();
 document.body.appendChild(mainContainer);
@@ -55,19 +55,19 @@ function startButton(){
 
 function Game(){
 
+
 var answerArray = [];
  for (var i = 0; i < word.length; i++) {
  answerArray[i] = "_";
 }
+   
 
 
 var remainingLetters = word.length;
 
 while(remainingLetters > 0){
   alert(answerArray.join(" "));
-    let arrayLetter = word_arr[i].split('');
-    
-
+  
     var guess = prompt("Guess a letter or click cancel to exit");
     
     if(guess==null){
@@ -93,4 +93,67 @@ alert(answerArray.join(" "));
 alert("the answer was " + word);
 }
 
+
+
+canvas =  function(){
+
+    myStickman = document.getElementById("stickman");
+    context = myStickman.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = "#fff";
+    context.lineWidth = 2;
+  };
+  
+    head = function(){
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI*2, true);
+      context.stroke();
+    }
+    
+  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke(); 
+}
+
+   frame1 = function() {
+     draw (0, 150, 150, 150);
+   };
+   
+   frame2 = function() {
+     draw (10, 0, 10, 600);
+   };
+  
+   frame3 = function() {
+     draw (0, 5, 70, 5);
+   };
+  
+   frame4 = function() {
+     draw (60, 5, 60, 15);
+   };
+  
+   torso = function() {
+     draw (60, 36, 60, 70);
+   };
+  
+   rightArm = function() {
+     draw (60, 46, 100, 50);
+   };
+  
+   leftArm = function() {
+     draw (60, 46, 20, 50);
+   };
+  
+   rightLeg = function() {
+     draw (60, 70, 100, 100);
+   };
+  
+   leftLeg = function() {
+     draw (60, 70, 20, 100);
+   };
+  
+  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
 
