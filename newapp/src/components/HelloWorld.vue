d26214ee52242aae7fd16bb997924b8ef210cc1b
sweekriti <sweekritiparajuli5@gmail.com>
2023-06-08 15:05:35 +0545

    started to learn vue

---
+++ newapp/src/components/HelloWorld.vue
@@ -0,0 +1,85 @@
+<!-- <script setup>
+  
+  
+  const count = ref(0);
+  
+  function increment(){
+    count.value++
+  }
+  
+  onMounted(()=>{
+    console.log(`the initial count is ${count.value}`)
+  })
+  
+</script>
+<template>
+  <button @click = "increment">
+    Count is : {{count}}
+  </button>
+</template> -->
+
+<!-- <script>
+export default{
+  data(){
+  return{
+        datas: [
+            'Hello', 
+            'World', 
+            
+        ]}
+    
+}
+  }
+
+</script>
+<template>
+  <ul>
+  <li v-for="item in datas" v-bind:key = "item.id">
+                {{item}}
+            </li>
+          </ul>
+</template> -->
+
+<script>
+import Table from './Table.vue'
+export default {
+  data(){    //data() is built-in function
+    return{
+      name:'',
+      address:'',
+      email:'',
+      
+      infos:[
+        'Name',
+        'Address',
+        'Email'
+      ]
+    }
+  },
+  components:{
+    Table
+  }
+
+}
+</script>
+<template>
+  <div class="form">
+  <h3>{{ infos[0] }}<input type="text" v-model="name"></h3>
+  <h3>{{ infos[1] }}<input type="text" v-model ="address"></h3>
+  <h3>{{ infos[2] }}<input type="text" v-model ="email"></h3>
+  <button type="button">Add</button>
+</div>
+<div>
+  <Table :name="name" :address="address" :email="email"/>
+</div>
+</template>
+<style>
+.form{
+  display: flex;
+  justify-content: space-around;
+}
+.form button{
+  border-radius: 10px;
+}
+</style>
+  
---
+++ newapp/src/components/Table.vue
@@ -0,0 +1,28 @@
+<template>
+    <table  border="1px">
+        <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>{{ name }}</td>
+            <td>{{address}}</td>
+            <td>{{email}}</td>
+        </tr>
+    </table>
+</template>
+<script>
+
+
+
+export default{
+    name:'table-vue',
+    props:{
+        name:String,
+        address:String,
+        email:String,
+        infos:Array,
+    }
+}
+</script>
